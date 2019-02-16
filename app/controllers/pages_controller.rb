f7eb5bc8c35a71e7d463de8907ac8d9a9e9e1a30
DmitrySalnikov <mejibkop.ru@mail.ru>
2019-02-16 17:54:03 +0300

    temp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,28 @@
 class PagesController < ApplicationController
 
-  def index
+  require 'nokogiri'
+  require 'open-uri'
 
+  def index
   end
 
   def create
-    # render plain: params[:main_form].inspect
-    @user_id = params.require(:main_form)[:user_id]
+    @user_id = params.require(:main_form).permit(:user_id)[:user_id]
+    if (false)
+      @error = "error"
+      render :index
+    end
+  end
+
+  def print_weight
+    user_id = params.permit(:user_id)[:user_id]
+    url = 'https://temp.medm-test.com/api/v3/' +
+      'provider/records/'+user_id+'/measurements/'+
+      'weights?sort_by=updated_at&order=desc&'+
+      'count=1&auth_token=09321865-5587-4ae5-b1ed-c5b31d107424'
+    xml = Nokogiri::XML(open(url));
+    render json: {response: xml.at_xpath("//value").content.to_s +
+      ' ' + xml.at_xpath("//units").content.to_s}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :pages
+  get "print_weight" => "pages#print_weight"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
